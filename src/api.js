fc0069637a22280c09586582547a3f2f00bf34c7
Lucas <lucas.ti@rodoviaria-poa.com.br>
2020-10-03 13:44:39 -0300

    correção de } dupla

--- src/api.js
+++ src/api.js
@@ -7,7 +7,7 @@ const headers = {
 }
 
 function getCountry(country) {
-    return fetch(`${path}/${country}}`, headers) // fetch é mais simples mas não é recomendado por questão de segurança
+    return fetch(`${path}/${country}`, headers) // fetch é mais simples mas não é recomendado por questão de segurança
         .then((response) => response.json()) // retorna o dado/país
 }
 
