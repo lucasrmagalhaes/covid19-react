23c178ec57629ad270be3696180b09a5d0d558ec
Lucas <lucas.ti@rodoviaria-poa.com.br>
2020-10-03 15:40:28 -0300

    css em js definido

---
+++ src/containers/Main/style.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+import { CardContent, Typography } from '../../../components'
+
+export const LabelStyled = styled(Typography)`
+    font-weight: 500;
+    font-size: 1.5rem;
+`
+
+export const ValueStyled = styled(Typography)`
+    font-weight: 400;
+    font-size: 2.5rem;
+`
+
+export const CardContentStyled = styled(CardContent)`
+    border-left: 8px solid ${ ({ color }) => color || '#5d78ff'};
+`
+
+export const CardPanelContentStyled = styled(CardContent)`
+    display: flex;
+    justify-content: space-between;
+    padding: 25px;
+`
+
+export const ItemStyled = styled.div`
+    display: flex;
+    justify-content: space-between;
+    min-width: 150px;
+`
