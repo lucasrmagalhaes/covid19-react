0599014024d31c4c6e8853fadc744a29c06d685d
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-10-03 00:33:02 -0300

    imports adicionados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from 'react'; 
+import { StylesProvider } from '@material-ui/styles'
+import { CssBaseline } from '@material-ui/core'
+import GlobalStyle from './commons/styles/global-style'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <StylesProvider injectFirst>
+      <CssBaseline />
+      <GlobalStyle />
+      <div>
+        Teste
+      </div>
+    </StylesProvider>
   );
 }
 
