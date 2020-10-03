5663ddfddac05072e82a7fda3e45990f3d9a405a
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-10-03 00:29:54 -0300

    pegando as configs e utilizando o babel

--- src/config-overrides.js
+++ src/config-overrides.js
@@ -1,2 +1,2 @@
-const { useBabelRc, overrider } = required('customize-cra')
+const { useBabelRc, override } = required('customize-cra')
 module.exports = overrider(useBabelRc()) // Pegar as configurações e utilizar o Babel.
