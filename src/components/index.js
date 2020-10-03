f21e447a22301abe4aff751272d3f0727d801448
Lucas <lucas.ti@rodoviaria-poa.com.br>
2020-10-03 15:25:37 -0300

    serviceWorker.register

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import { Card, CardContent } from './Card'
+import Button from './Button'
+import Grid from './Grid'
+import Typography from './Typography'
+import Skeleton from './Skeleton'
+import Select from './Select'
+import MenuItem from './MenuItem'
+
+export {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+  Skeleton,
+  Select,
+  MenuItem
+}
