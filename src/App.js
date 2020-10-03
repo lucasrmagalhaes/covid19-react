936eeecf2037ac897583b2823debadadd71be50a
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-10-03 00:47:15 -0300

    main adicionado

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { StylesProvider } from '@material-ui/styles'
 import { CssBaseline } from '@material-ui/core'
 import GlobalStyle from './commons/styles/global-style'
+import Main from './containers/Main'
 
 function App() {
   return (
     <StylesProvider injectFirst>
       <CssBaseline />
       <GlobalStyle />
+      <Main />
       <div>
         Teste
       </div>
