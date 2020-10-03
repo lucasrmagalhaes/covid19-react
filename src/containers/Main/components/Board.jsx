383cf9795bc353dd9663b7723702863be041b340
Lucas <lucas.ti@rodoviaria-poa.com.br>
2020-10-03 15:27:37 -0300

    facilitando as importacoes

---
+++ src/containers/Main/components/Board.jsx
@@ -0,0 +1,30 @@
+import React, { memo } from 'react'
+import PropTypes from 'pro-types'
+import { Grid, Skeleton } from '../../../components'
+import Card from './Card'
+
+function Board ({ data }) { // Cards sobre o Covid-19 irão ficar aqui
+    
+    // Dados que a API retorna
+    const { cases, todayDeaths, recovered, deaths, todayCases } = data 
+
+/* 
+ * Função para retornar o valor ou uma barra cinza
+ * Skeleton - melhorar a interface de loading
+ */
+    const getValue = (value) => value ? value : <Skeleton variant="text" width={182} height={60} />
+
+    return (
+        <Grid container spacing={4}>
+            <Grid item xs={12} md={3}>
+                <Card value={getValue(cases)} label="Total de Casos" color="#5d78ff" />
+            </Grid>
+        </Grid>
+    )
+}
+
+
+
+
+
+
---
+++ src/containers/Main/components/Card.jsx
@@ -0,0 +1,23 @@
+// Card para receber os dados
+
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+import { Card as CardUI } from '../../../components'
+import {
+    LabelStyled,
+    ValueStyled,
+    CardContentStyled
+} from './style'
+
+function Card({ value, label, color }) {
+    return (
+        <CardUI>
+            <CardContentStyled color={color}>
+                <ValueStyled>{value}</ValueStyled>
+                <LabelStyled>{label}</LabelStyled>
+            </CardContentStyled>
+        </CardUI>
+    )
+}
+
+export default memo (Card)
