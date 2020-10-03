b241547b2d4ce84f819918a10fc9ea576daf7a96
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-10-02 23:14:42 -0300

    arquivo para sobrescrever as configurações do react

--- src/config-overrides.js
+++ src/config-overrides.js
@@ -1,2 +1,2 @@
 const { useBabelRc, overrider } = required('customize-cra')
-module.exports = overrider(useBabelRc()) // pegar as configurações e utilizar o Babel.
+module.exports = overrider(useBabelRc()) // Pegar as configurações e utilizar o Babel.
