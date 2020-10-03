34dd5d27bc820f4eb97e2922e7a45f217a9c35ea
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-10-03 00:31:41 -0300

    imports, css e export definidos

---
+++ src/commons/styles/global-style.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components'
+import CovidImg from '../../assets/images/covid.jpeg'
+
+const globalStyle = createGlobalStyle`
+    * {
+        outline: none;
+        box-sizing: border-box;
+    }
+
+    body {
+        line-height: normal;
+    }
+
+    html, body {
+        width: 100%;
+        min-height: 100%;
+        display: flex;
+        padding: 0;
+        margin: 0;
+    }
+
+    #root {
+        background: url(${CovidImg});
+        height: 100%;
+        width: 100%;
+        background-size: cover;
+        background-position: center center;
+    }
+    `
+
+    export default globalStyle
