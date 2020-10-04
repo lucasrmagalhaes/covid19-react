3abf051d2c54a13ff56a331911e5576c85ae81d2
Lucas <lucas.ti@rodoviaria-poa.com.br>
2020-10-04 13:04:33 -0300

    pwa

---
+++ public/worker.js
@@ -0,0 +1,51 @@
+/* 
+ * Executando o JS de forma separada em outra trade de forma assincrona.
+ * Sem acesso ao DOM e não vai criar bloqueio ao user.
+ * JS só possui uma trade.
+ */
+
+var CACHE_NAME = 'covid19-pwa'; // offline e cacheado - vantagem pwa
+var urlsToCache = [
+  '/'
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  var cacheWhitelist = ['pwa-task-manager'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
