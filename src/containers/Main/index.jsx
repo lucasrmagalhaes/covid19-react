685536624655b9da5000db873416753664df26e9
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-10-04 01:30:50 -0300

    icones adicionados

--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,11 +1,13 @@
 import React, { memo, useState, useCallback, useEffect } from 'react'
 import Api from '../../api'
 import Board from './components/Board'
+import Panel from './components/Panel'
 import { ContainerStyled } from './style'
 
 function Main() {
   const [data, setData] = useState({})
   const [country, setCountry] = useState('brazil')
+  const updateAt = new Date().toLocaleString()
 
   const getCovidData = useCallback((country) => {
     Api.getCountry(country)
@@ -16,10 +18,21 @@ function Main() {
     getCovidData(country)
   }, [getCovidData, country])
 
+  const handleChange = ({ target }) => {
+    const country = target.value
+    setCountry(country)
+  } 
+
   return (
     <ContainerStyled>
       <div className="mb-2">
-
+        <Panel
+          data={data}
+          updateAt={updateAt} // Data de atualização do próprio browser
+          onChange={handleChange}
+          country={country}
+          getCovidData={getCovidData}
+        />
       </div>
       <Board data={data} />
     </ContainerStyled>
