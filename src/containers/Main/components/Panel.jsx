844263881ce6e0225b90f62349ae0732f0bd5855
Lucas <lucas.ti@rodoviaria-poa.com.br>
2020-10-04 13:00:59 -0300

    api e constantes adicionados

--- src/containers/Main/components/Panel.jsx
+++ src/containers/Main/components/Panel.jsx
@@ -60,8 +60,8 @@ function Panel({ updateAt, onChange, data, country, getCoviddata }) {
         <Card>
             <CardPanelContentStyled>
                 <div>
-                    <Typography variant="h5" component="span" color="primary">Covid-19</Typography>
-                    <Typography variant="h6" component="span" color="primary">Painel Coronavírus</Typography>
+                    <Typography variant="h5" component="span" color="primary">Coronavírus (COVID-19) </Typography>
+                    <Typography variant="h6" component="span" color="primary">Visão Geral </Typography>
                     <Typography variant="body2" component="span" color="primary">Atualizado em: {updateAt}</Typography>
                     <div className="pt-2">
                         <Select onChange={onChange} value={country}>
