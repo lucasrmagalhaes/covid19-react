fc121b07e460c3397d51458dcbecc83a71d90e7f
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-10-04 01:30:29 -0300

    array de paises criado

---
+++ src/commons/constants/countries.js
@@ -0,0 +1,36 @@
+import BrazilFlag from '../../assets/images/brazil.png'
+import ChinaFlag from '../../assets/images/china.png'
+import GermanyFlag from '../../assets/images/germany.png'
+import ItalyFlag from '../../assets/images/italy.png'
+import UsaFlag from '../../assets/images/usa.png'
+ 
+// Array com os países: Nome, valor API e foto(bandeira).
+const COUNTRIES = [
+    {
+        label: 'Alemanha',
+        value: 'germany',
+        flag: GermanyFlag
+    },
+    {
+        label: 'Brasil',
+        value: 'brazil',
+        flag: BrazilFlag
+    },
+    {
+        label: 'China',
+        value: 'china',
+        flag: ChinaFlag
+    },
+    {
+        label: 'Estados Unidos',
+        value: 'usa',
+        flag: UsaFlag
+    },
+    {
+        label: 'Itália',
+        value: 'italy',
+        flag: ItalyFlag
+    }
+]
+
+export default COUNTRIES
